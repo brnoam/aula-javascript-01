d261fd8f9889f0eb05e92f370d7ad7b325cace21
unknown <brnoamrr@gmail.com>
2021-09-16 23:42:40 -0300

    envio atv javascritp

---
+++ aulajavascript/script.js.js
@@ -0,0 +1,36 @@
+var nome = null;
+
+nome = "Bruno";
+
+console.log("meu nome é :" + nome );
+
+
+
+
+var carro = null;
+
+carro = "Toyota";
+
+console.log("Meu carro é um : " + carro);
+
+
+let cor = null;
+
+cor = "Azul";
+
+console.log ("a cor é : " + cor);
+
+
+
+
+let estado = null;
+
+estado = "Pernambuco";
+
+console.log ("Meu estado é : " + estado);
+
+
+const CPF = 123456;
+
+
+console.log ("O CPF é : " + CPF); 
